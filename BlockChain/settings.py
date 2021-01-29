db59eb00ac8fb9940da7da42f77f4b5414f275ec
Полина <vaganowa.p@yandex.ru>
2021-01-29 19:33:03 +0300

    'second'

--- BlockChain/settings.py
+++ BlockChain/settings.py
@@ -119,10 +119,10 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = False
+STATIC_ROOT = 'staticfiles'
 
-DISABLE_COLLECTSTATIC=1
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
