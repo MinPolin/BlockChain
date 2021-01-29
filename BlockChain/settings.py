aa839a43678418d756c6b137ab455f0eaf3e54b2
Полина <vaganowa.p@yandex.ru>
2021-01-29 19:28:22 +0300

    'second'

--- BlockChain/settings.py
+++ BlockChain/settings.py
@@ -120,7 +120,7 @@ USE_L10N = True
 
 USE_TZ = False
 
-
+DISABLE_COLLECTSTATIC=1
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = '/static/'
