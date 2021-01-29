40cba8fcbfcaa98547f55e3e0efdef55d8165584
Полина <vaganowa.p@yandex.ru>
2021-01-29 20:09:23 +0300

    'second'

--- BlockChain/settings.py
+++ BlockChain/settings.py
@@ -119,21 +119,19 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = False
-STATIC_ROOT = 'staticfiles'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-
+STATIC_ROOT = 'staticfiles'
 
 
 # Extra lookup directories for collectstatic to find static files
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
 import dj_database_url
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
